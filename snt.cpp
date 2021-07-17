0175bf6119327e00fe4f0e9d4aa3c8004f513627
Tùng Đinh <tungdvx@gmail.com>
2021-07-17 12:25:47 +0700

    add file cpp

---
+++ snt.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+int snt (int n)
+{
+    if (n < 2)
+        return 0;
+
+    if (n == 2)
+        return 1:
+
+    for (int i = 0; i < sqrt(n); i ++)
+        if (n % i == 0)
+            return 0;
+    return 1;
+}
+
+int main ()
+{
+    if (snt(n))
+        cout << n << " la so nguyen to!\n";
+}
